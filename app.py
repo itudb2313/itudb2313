66bf62f04d69e66a02fad9fa1d8b9d1a59e31383
itu-itis22-yilmazor21 <yilmazor21@itu.edu.tr>
2023-11-06 20:43:54 +0300

    Create docstrings on app.py file

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+"""
+This is the main file that will be run by the server.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """
+    This is the main page of the website.
+    """
     return "<p>Hello, World!</p>"
 
 @app.route("/db/<name>")
 def hello(name):
-    # normally you should sanitize using escape()
+    """
+    This is a page that will take a name as a parameter and return a greeting.
+    
+    - normally you should sanitize using escape()
+    """
+
     return f"Hello, {name}!"
