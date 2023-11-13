fb82784fbd6e444998bc04dddb385ace61d65955
itu-itis22-yilmazor21 <yilmazor21@itu.edu.tr>
2023-11-13 20:02:25 +0300

    Update progress

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ with app.app_context():
 def hello_world():
     return render_template("index.html")
 
-@app.route("/db/<name>")
-def hello(name):
-    return f"Hello, {name}!"
-
 @app.route("/products")
 def products():
     return render_template("products.html", products=db.get_all_products())
