d49a1c8f5f2a001a54020979a7158b68cac7292f
Muhammed Necati Polat <mnecatipolat@gmail.com>
2023-11-13 14:27:28 +0300

    fix get all products function

--- products.py
+++ products.py
@@ -2,7 +2,10 @@ import mysql.connector
 from database_config import get_database_config
 
 def get_all_products():
-    with mysql.connector.connect(**get_database_config()) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM product")
-            return cursor.fetchall()
+    connection = mysql.connector.connect(**get_database_config())
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM product")
+    products = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return products
