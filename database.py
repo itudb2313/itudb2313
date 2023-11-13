068026738530157458e7a2cad57e645436cf5d92
itu-itis22-yilmazor21 <yilmazor21@itu.edu.tr>
2023-11-13 15:31:15 +0300

    Fix formatting

--- database.py
+++ database.py
@@ -6,51 +6,70 @@ class Database:
         self.connection = dbapi.connect(
             host="localhost",
             port=3306,
-            user="root", 
-            password= "test",
-            database="AutomotiveBusiness"
+            user="root",
+            password="test",
+            database="AutomotiveBusiness",
         )
 
-    def selectAllEmployee(self):
-        selectAllEmployees = """SELECT * FROM employee"""
+    def select_all_employees(self):
+        query = """SELECT * FROM employee"""
         try:
             cursor = self.connection.cursor()
-            cursor.execute(selectAllEmployees)
-            return cursor
+            cursor.execute(query)
+            # return cursor
         except dbapi.DatabaseError:
             self.connection.rollback()
         finally:
             cursor.close()
-            
 
-    def insertEmployee(self, employee):
-        insertEmployeeQuery = """INSERT INTO employee (employee_id, store_id, firstname, lastname, dof, phone, email, status, salary, street, city, country) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)"""
+    def insert_employee(self, employee):
+        query = """INSERT INTO employee (employee_id, store_id, firstname,
+        lastname, dof, phone, email, status, salary, street, city, country)
+        VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)"""
 
         try:
             cursor = self.connection.cursor()
-            cursor.execute(insertEmployeeQuery, (employee.employee_id, employee.store_id, employee.firstname, employee.lastname, employee.dof, employee.phone, employee.email, employee.status, employee.salary, employee.street, employee.city, employee.country))
+            cursor.execute(
+                query,
+                (
+                    employee.employee_id,
+                    employee.store_id,
+                    employee.firstname,
+                    employee.lastname,
+                    employee.dof,
+                    employee.phone,
+                    employee.email,
+                    employee.status,
+                    employee.salary,
+                    employee.street,
+                    employee.city,
+                    employee.country,
+                ),
+            )
             self.connection.commit()
         except dbapi.DatabaseError:
             self.connection.rollback()
         finally:
             cursor.close()
 
-    def selectAllRises(self):
-        selectAllRises = """SELECT * FROM rise_archive"""
+    def select_all_rises(self):
+        query = """SELECT * FROM rise_archive"""
         try:
             cursor = self.connection.cursor()
-            cursor.execute(selectAllRises)
-            return cursor
+            cursor.execute(query)
+            # return cursor
         except dbapi.DatabaseError:
             self.connection.rollback()
         finally:
             cursor.close()
 
-    def insertRise(self):
-        insertRiseQuery = """INSERT INTO rise_archive (rise_id, amount_by_percent, rise_date, rise_state) VALUES (%s, %s,%s, %s)"""
+    def insert_rise(self):
+        query = """INSERT INTO rise_archive (rise_id, amount_by_percent,
+        rise_date, rise_state) VALUES (%s, %s,%s, %s)"""
+
         try:
             cursor = self.connection.cursor()
-            cursor.execute(insertRiseQuery)
+            cursor.execute(query)
             self.connection.commit()
         except dbapi.DatabaseError:
             self.connection.rollback()
