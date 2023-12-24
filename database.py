4e7e468c2815e66d4351d26a9b49af5f3869f9c0
ardasarac44 <saracoglu21@itu.edu.tr>
2023-12-24 18:50:28 +0300

    insert, and edit finished style added

--- database.py
+++ database.py
@@ -182,7 +182,58 @@ class Database:
             self.connection.rollback()
         finally:
             cursor.close()
-    
+    def update_store(self, store_id, employee_id, store_name, phone, street, city, country, email, post_code):
+        query = """UPDATE store SET employee_id=%s, store_name='%s', phone='%s', street='%s', city='%s', country='%s', email='%s', post_code=%s WHERE store_id=%s"""
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(
+                query,
+                (
+                    employee_id,
+                    store_name,
+                    phone,
+                    street,
+                    city,
+                    country,
+                    email,
+                    post_code,
+                    store_id,
+                ),
+            )
+            self.connection.commit()
+        except dbapi.DatabaseError:
+            print("error")
+            self.connection.rollback()
+        finally:
+            cursor.close()
+
+
+    def insert_store(self, store_id, employee_id, store_name, phone, street, city, country, email, post_code):
+        query = """INSERT INTO store (store_id, employee_id, store_name, phone, street, city, country, email, post_code)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(
+                query,
+                (
+                    store_id,
+                    employee_id,
+                    store_name,
+                    phone,
+                    street,
+                    city,
+                    country,
+                    email,
+                    post_code,
+                ),
+            )
+            self.connection.commit()
+        except dbapi.DatabaseError:
+            print("error")
+            self.connection.rollback()
+        finally:
+            cursor.close()
+
     def get_all_stores_table(self,order_opt = "store_id",page_number = 1):
         query = "SELECT * FROM store order by "+ order_opt +" limit 20 offset "+ str((int(page_number)-1)*20)
         try:
--- endpoints/stores.py
+++ endpoints/stores.py
@@ -29,33 +29,58 @@ def delete_store():
     db.delete_store(store_id)
     return "Deleted"
     
-@stores_bp.route("/stores/update", methods=["POST"])
+@stores_bp.route("/stores/update", methods=["GET", "POST"])
 def edit_store():
     db = current_app.config.get("db")
-    data = request.form.get("data")[1:-1].split(",")
-    print(data)
-    return f"""
-            <tr>
-            <td>{data[0]}</td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><input class="edit" type="text" id="fname" name="fname"></td>
-            <td><button class="btn btn-primary">Update</button></td>
-            <td><button class="btn btn-primary">Cancel</button></td>
-            </tr>"""
+    if(request.method == "GET"):
+        data=request.args.get("data")[1:-1].split(",")
+        l=[]
+        for i in data:
+            l.append(i.replace("'", ''))
+        return f"""
+                <tr id="edit">
+                <td id= "store_id_cell">{l[0]}</td>
+                <td><input class="edit" type="text"   name="emp_id" value="{l[1]}"></td>
+                <td><input class="edit" type="text"   name="store_name" value="{l[2]}"></td>
+                <td><input class="edit" type="text"   name="phone" value="{l[3]}"></td>
+                <td><input class="edit" type="text"   name="street" value="{l[4]}"></td>
+                <td><input class="edit" type="text"   name="city" value="{l[5]}"></td>
+                <td><input class="edit" type="text"   name="country" value="{l[6]}"></td>
+                <td><input class="edit" type="text"   name="email" value="{l[7]}"></td>
+                <td><input class="edit" type="text"   name="post_code" value="{l[8]}"></td>
+                <td><button class="btn btn-primary" id="{l[0]}" hx-post="/stores/update" hx-include="closest tr" hx-target="#data" onclick="update_trigger(this)">Update</button></td>
+                <td><button class="btn btn-primary" id="cancel-button" hx-get = "/stores_table" 
+                hx-trigger="click" hx-target="#data" onclick="cancel_edit(this)">Cancel</button></td>
+                </form>
+                </tr>"""
+    if(request.method == "POST"):
+        db = current_app.config.get("db")
+        store_id = request.form.get("store_id")
+        employee_id = request.form.get("emp_id")
+        store_name = request.form.get("store_name")
+        phone = request.form.get("phone")
+        street = request.form.get("street")
+        city = request.form.get("city")
+        country = request.form.get("country")
+        email = request.form.get("email")
+        post_code = request.form.get("post_code")
 
+        db.update_store(store_id, employee_id, store_name, phone, street, city, country, email, post_code)
+        order_opt = request.form.get("order_opt")
+        page_number = request.form.get("page_number")
+        stores = db.get_all_stores_table(order_opt=order_opt, page_number=page_number)
+        return render_template("stores_table.html", 
+                                stores=stores, 
+                                headers=db.get_stores_columns())
+        
 
 
 
 @stores_bp.route("/stores/insert", methods=["POST"])
 def insert_store():
     db = current_app.config.get("db")
-    employee_id = request.form.get("employee_id")
+    store_id = request.form.get("store_id")
+    emp_id = request.form.get("emp_id")
     store_name = request.form.get("store_name")
     phone = request.form.get("phone")
     street = request.form.get("street")
@@ -63,9 +88,26 @@ def insert_store():
     country = request.form.get("country")
     email = request.form.get("email")
     post_code = request.form.get("post_code")
-    #db.insert_store(store_name, store_address, store_country, store_city, store_phone, store_email, store_manager)
-    #return redirect(url_for("stores"))
-    return "OK"
+    print(store_id, emp_id, store_name, phone, street, city, country, email, post_code)
+    db.insert_store(store_id, emp_id, store_name, phone, street, city, country, email, post_code)
+    return f"""<tr>
+                    <td>{store_id}</td>
+                    <td>{emp_id}</td>
+                    <td>{store_name}</td>
+                    <td>{phone}</td>
+                    <td>{street}</td>
+                    <td>{city}</td>
+                    <td>{country}</td>
+                    <td>{email}</td>
+                    <td>{post_code}</td>
+                    <td>
+                        <button class="btn btn-primary" hx-post="/stores/delete" hx-vals='"store_id": "13"' hx-trigger="click" hx-target="this" hx-swap="outerHTML" onclick="delete_trigger(this)">Delete</button>
+                    </td>
+                    <td>
+                        <button class="btn btn-primary" onclick="edit_trigger(this)">Edit</button>     
+                    </td> 
+                </tr>
+                    """
 
 @stores_bp.route("/stores_table", methods=["GET"])
 def stores_table():
