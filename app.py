735bfb40919ce5d5f36aeaf317ba65e7f6252164
ardasarac44 <saracoglu21@itu.edu.tr>
2023-12-25 00:01:38 +0300

    app.py merged

--- app.py
+++ app.py
@@ -38,61 +38,6 @@ def categories():
 
 
 
-@app.route("/products")
-def get_products():
-    searchword = request.args.get("search", "")
-    searchword = "%" + searchword + "%"
-    return render_template("products.html", products=db.get_products(search=searchword))
-
-
-@app.route("/insert_product", methods=["GET"])
-def get_insert_product_page():
-    return render_template(
-        "insert_product.html",
-        providers=db.get_providers(),
-        categories=db.select_all_categories(),
-    )
-
-
-@app.route("/delete_product", methods=["POST"])
-def delete_product():
-    product_id = request.json.get("product_id")
-    db.delete_product(product_id)
-    return redirect(url_for("get_products"))
-
-
-@app.route("/insert_product", methods=["POST"])
-def insert_product():
-    product_name = request.form["name"]
-    model = request.form["model"]
-    year = request.form["year"]
-    color = request.form["color"]
-    price = request.form["price"]
-    km = request.form["mileage"]
-    category_id = request.form["category_id"]
-    provider_id = request.form["provider_id"]
-    if db.insert_product(
-        product_name, model, year, color, price, km, category_id, provider_id
-    ):
-        return redirect(url_for("get_products"))
-    else:
-        return ["Error"]
-
-
-@app.route("/providers")
-def get_providers():
-    searchword = request.args.get("search", "")
-    searchword = "%" + searchword + "%"
-    start_debt = request.args.get("from", "")
-    end_debt = request.args.get("to", "")
-    return jsonify(db.get_providers(search=searchword, start=start_debt, to=end_debt))
-
-
-@app.route("/providers/countries")
-def get_proiveder_countries():
-    return jsonify(db.get_provider_countries())
-
-
 # Example code snippet for json data transfer. Do not remove.
 @app.route("/process_json", methods=["POST"])
 def process_json():
